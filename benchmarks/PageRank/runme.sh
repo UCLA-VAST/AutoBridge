a8322625a3d454e9274392557896aa6cad36a926
Licheng-Guo <lichengguo777@gmail.com>
2020-12-24 22:31:44 -0800

    add runme.sh for Page-Rank

---
+++ benchmarks/PageRank/runme.sh
@@ -0,0 +1,13 @@
+
+# To run AutoBridge:
+./ab_pagerank_u280.py | tee autobridge.log
+
+# in case of pyverilog bug, run again
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+  echo "Detecting error, Rerun AutoBridge..."
+  ./ab_pagerank_u280.py | tee autobridge.log
+fi
+
+# To run Vitis, we have to first package the results using the TAPA compiler. We provided the XO that has already been packaged (PageRank_FP1.xo) if you do not have TAPA already installed. 
+./run_vitis.sh
+
