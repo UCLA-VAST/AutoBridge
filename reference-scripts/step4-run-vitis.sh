cd3d7ed099458957f8533e353ce26d45b7c97238
Licheng-Guo <lichengguo777@gmail.com>
2020-12-30 19:24:38 -0800

    update reference script for step 4

--- reference-scripts/step4-run-vitis.sh
+++ reference-scripts/step4-run-vitis.sh
@@ -1,37 +1,56 @@
-OUTPUT_DIR=./output
+OUTPUT_DIR="./vitis_run"
 
 # name of the top function
-TOP=YOUR_TOP_NAME
+TOP="YOUR_TOP_NAME"
 
 # choose the target device
 PLATFORM=xilinx_u250_xdma_201830_2 
 #PLATFORM=xilinx_u280_xdma_201920_3 
 
-XO=YOUR_XO_NAME
+XO="YOUR_XO_NAME"
 
 # For different approaches see UG904-vivado-implementation
 STRATEGY="Default" 
 #STRATEGY="EarlyBlockPlacement" 
 
-output_dir="$(pwd)/vitis_run"
+# remove the unused '--connectivity.sp' option for v++ if some DDRs are not used 
+# Example: if we map p1 to DDR 3 and p2 to DDR 0
+#
+# void kernel0(ap_uint<512> *p1, ap_uint<512> *p2)
+# {
+#   #pragma HLS INTERFACE m_axi port=p1 offset=slave bundle=gmem_A
+#   #pragma HLS INTERFACE m_axi port=p2 offset=slave bundle=gmem_B
+# 
+#   load_p1 (p1, ...);
+#   load_p2 (p2, ...);
+# }
+#
+# ARG_FOR_DDR_0=p2
+# ARG_FOR_DDR_3=p1
+# Should remove '--connectivity.sp' for DDR1 and DDR2
+
+ARG_FOR_DDR_1="YOUR_HLS_ARGUMENT_NAME_FOR_DDR_1"
+ARG_FOR_DDR_2="YOUR_HLS_ARGUMENT_NAME_FOR_DDR_2"
+ARG_FOR_DDR_3="YOUR_HLS_ARGUMENT_NAME_FOR_DDR_3"
+ARG_FOR_DDR_4="YOUR_HLS_ARGUMENT_NAME_FOR_DDR_4"
 
 v++ \
   --link \
-  --output "${output_dir}/${TOP}_${PLATFORM}.xclbin" \
+  --output "${OUTPUT_DIR}/${TOP}_${PLATFORM}.xclbin" \
   --kernel ${TOP} \
   --platform ${PLATFORM} \
   --target hw \
   --report_level 2 \
-  --temp_dir "${output_dir}/${TOP}_${PLATFORM}.temp" \
+  --temp_dir "${OUTPUT_DIR}/${TOP}_${PLATFORM}.temp" \
   --optimize 3 \
   --connectivity.nk ${TOP}:1:${TOP}_1 \
   --max_memory_ports ${TOP} \
   --save-temps \
   ${XO} \
-  --connectivity.sp ${TOP}_1.PORT_NAME_1:DDR[0] \ # change according to your design
-  --connectivity.sp ${TOP}_1.PORT_NAME_2:DDR[1] \ # change according to your design
-  --connectivity.sp ${TOP}_1.PORT_NAME_3:DDR[2] \ # change according to your design
-  --connectivity.sp ${TOP}_1.PORT_NAME_4:DDR[3] \ # change according to your design
+  --connectivity.sp ${TOP}_1.ARG_FOR_DDR_1:DDR[0] \
+  --connectivity.sp ${TOP}_1.ARG_FOR_DDR_2:DDR[1] \
+  --connectivity.sp ${TOP}_1.ARG_FOR_DDR_3:DDR[2] \
+  --connectivity.sp ${TOP}_1.ARG_FOR_DDR_4:DDR[3] \
   --kernel_frequency 330 \
   --vivado.prop run.impl_1.STEPS.PLACE_DESIGN.ARGS.DIRECTIVE=$STRATEGY \
   --vivado.prop run.impl_1.STEPS.OPT_DESIGN.TCL.PRE=$CONSTRAINT
