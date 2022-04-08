9c4d0a53614e49f615462a024bd79b9301bfe8b8
Licheng-Guo <lichengguo777@gmail.com>
2022-04-07 19:31:32 -0700

    [floorplan] two priority modes for eight-way partition

--- src/autobridge/Floorplan/EightWayPartition.py
+++ src/autobridge/Floorplan/EightWayPartition.py
@@ -19,11 +19,53 @@ def eight_way_partition(
   slot_manager: SlotManager,
   grouping_constraints: List[List[Vertex]],
   pre_assignments: Dict[Vertex, Slot],
-  ref_usage_ratio: float,
-  max_search_time: int = 600,
-  max_usage_ratio: float = 0.84,
-  ref_slr_width_limit: int = 10000,
+  min_area_limit: float = 0.65,
+  max_area_limit: float = 0.85,
+  min_slr_width_limit: int = 10000,
   max_slr_width_limit: int = 15000,
+  max_search_time: int = 600,
+  strategy: str = 'AREA_PRIORITIZED',
+) -> Dict[Vertex, Slot]:
+  _logger.info('Use the %s strategy in floorplanning', strategy)
+
+  if strategy == 'AREA_PRIORITIZED':
+    return eight_way_partition_area_prioritized(
+      init_v2s,
+      slot_manager,
+      grouping_constraints,
+      pre_assignments,
+      min_area_limit,
+      max_area_limit,
+      min_slr_width_limit,
+      max_slr_width_limit,
+      max_search_time,
+    )
+  elif strategy == 'SLR_CROSSING_PRIORITIZED':
+    return eight_way_partition_slr_crossing_prioritized(
+      init_v2s,
+      slot_manager,
+      grouping_constraints,
+      pre_assignments,
+      min_area_limit,
+      max_area_limit,
+      min_slr_width_limit,
+      max_slr_width_limit,
+      max_search_time,
+    )
+  else:
+    raise NotImplementedError('unrecognized strategy: %s', strategy)
+
+
+def eight_way_partition_area_prioritized(
+  init_v2s: Dict[Vertex, Slot],
+  slot_manager: SlotManager,
+  grouping_constraints: List[List[Vertex]],
+  pre_assignments: Dict[Vertex, Slot],
+  min_area_limit: float,
+  max_area_limit: float,
+  min_slr_width_limit: int,
+  max_slr_width_limit: int,
+  max_search_time: int,
 ) -> Dict[Vertex, Slot]:
   """
   adjust the max_usage_ratio if failed
@@ -32,30 +74,30 @@ def eight_way_partition(
 
   # start from the largest allowed ratio and search downwards. Prune if fail.
   # for curr_max_usage in reversed(float_range(ref_usage_ratio, max_usage_ratio, 0.02)):
-  lo = ref_usage_ratio
-  hi = max_usage_ratio
+  lo = min_area_limit
+  hi = max_area_limit
   assert lo < hi
 
   while (1):
-    curr_max_usage = (lo + hi) / 2
+    curr_area_limit = (lo + hi) / 2
 
-    _logger.info(f'Attempt eight way partition with max_usage_ratio {curr_max_usage}')
+    _logger.info(f'Attempt eight way partition with area limit of {curr_area_limit}')
 
     curr_best_v2s = _binary_search_slr_crossing_limit(
       init_v2s,
       slot_manager,
       grouping_constraints,
       pre_assignments,
-      curr_max_usage,
-      ref_slr_width_limit,
+      curr_area_limit,
+      min_slr_width_limit,
       max_slr_width_limit,
       max_search_time,
     )
     if curr_best_v2s:
       best_v2s = curr_best_v2s
-      hi = curr_max_usage
+      hi = curr_area_limit
     else:
-      lo = curr_max_usage
+      lo = curr_area_limit
 
     if best_v2s:
       if hi - lo < 0.03:
@@ -65,7 +107,7 @@ def eight_way_partition(
         break
 
   if not best_v2s:
-    _logger.info(f'eight way partition failed with max_usage_ratio {curr_max_usage} and slr_width_limit {max_slr_width_limit}')
+    _logger.info(f'eight way partition failed with max_usage_ratio {curr_area_limit} and slr_width_limit {max_slr_width_limit}')
     return {}
 
   log_resource_utilization(best_v2s)
@@ -77,10 +119,10 @@ def _binary_search_slr_crossing_limit(
   slot_manager: SlotManager,
   grouping_constraints: List[List[Vertex]],
   pre_assignments: Dict[Vertex, Slot],
-  max_usage_ratio,
-  min_slr_width_limit,
-  max_slr_width_limit,
-  max_search_time,
+  area_limit: float,
+  min_slr_width_limit: int,
+  max_slr_width_limit: int,
+  max_search_time: int,
 ) -> Dict[Vertex, Slot]:
 
   curr_best_v2s = {}
@@ -95,13 +137,13 @@ def _binary_search_slr_crossing_limit(
     _logger.info(f'Try slr_width_limit {curr_slr_limit}')
 
     v2s = _eight_way_partition(
-      init_v2s, 
-      grouping_constraints, 
-      pre_assignments, 
-      slot_manager, 
-      max_usage_ratio, 
-      max_search_time, 
+      init_v2s,
+      grouping_constraints,
+      pre_assignments,
+      slot_manager,
+      area_limit,
       curr_slr_limit,
+      max_search_time,
     )
 
     if v2s:
@@ -119,10 +161,121 @@ def _binary_search_slr_crossing_limit(
         break
 
   if curr_best_v2s:
-    _logger.info(f'Found solution with max_usage_ratio {max_usage_ratio} and slr_width_limit {curr_min_slr_limit}')
+    _logger.info(f'Found solution with area limit {area_limit} and slr_width_limit {curr_min_slr_limit}')
 
   return curr_best_v2s
 
+
+def eight_way_partition_slr_crossing_prioritized(
+  init_v2s: Dict[Vertex, Slot],
+  slot_manager: SlotManager,
+  grouping_constraints: List[List[Vertex]],
+  pre_assignments: Dict[Vertex, Slot],
+  min_area_limit: float,
+  max_area_limit: float,
+  min_slr_width_limit: int,
+  max_slr_width_limit: int,
+  max_search_time: int,
+) -> Dict[Vertex, Slot]:
+  """
+  adjust the max_usage_ratio if failed
+  """
+  best_v2s = {}
+
+  # start from the largest allowed ratio and search downwards. Prune if fail.
+  # for curr_max_usage in reversed(float_range(ref_usage_ratio, max_usage_ratio, 0.02)):
+  lo = min_slr_width_limit
+  hi = max_slr_width_limit
+  assert lo < hi
+
+  while (1):
+    curr_slr_limit = (lo + hi) / 2
+
+    _logger.info(f'Attempt eight way partition with slr_crossing_limit {curr_slr_limit}')
+
+    curr_best_v2s = _binary_search_area_limit(
+      init_v2s,
+      slot_manager,
+      grouping_constraints,
+      pre_assignments,
+      curr_slr_limit,
+      min_area_limit,
+      max_area_limit,
+      max_search_time,
+    )
+    if curr_best_v2s:
+      best_v2s = curr_best_v2s
+      hi = curr_slr_limit
+    else:
+      lo = curr_slr_limit
+
+    if best_v2s:
+      if hi - lo < 700:
+        break
+    else:
+      if hi - lo < 500:
+        break
+
+  if not best_v2s:
+    _logger.info(f'eight way partition failed with area limit {max_area_limit} and slr_width_limit {curr_slr_limit}')
+    return {}
+
+  log_resource_utilization(best_v2s)
+  return best_v2s
+
+
+def _binary_search_area_limit(
+  init_v2s: Dict[Vertex, Slot],
+  slot_manager: SlotManager,
+  grouping_constraints: List[List[Vertex]],
+  pre_assignments: Dict[Vertex, Slot],
+  slr_width_limit,
+  min_area_limit,
+  max_area_limit,
+  max_search_time,
+) -> Dict[Vertex, Slot]:
+
+  curr_best_v2s = {}
+
+  # binary search:
+  hi = max_area_limit
+  lo = min_area_limit
+  assert lo < hi
+
+  while (1):
+    curr_area_limit = (hi + lo) / 2
+    _logger.info(f'Try area limit {curr_area_limit}')
+
+    v2s = _eight_way_partition(
+      init_v2s,
+      grouping_constraints,
+      pre_assignments,
+      slot_manager,
+      curr_area_limit,
+      slr_width_limit,
+      max_search_time,
+    )
+
+    if v2s:
+      curr_best_v2s = v2s
+      curr_min_usage = curr_area_limit
+      hi = curr_area_limit
+    else:
+      lo = curr_area_limit
+
+    if curr_best_v2s:
+      if hi - lo < 0.015:
+        break
+    else:
+      if hi - lo < 0.01:
+        break
+
+  if curr_best_v2s:
+    _logger.info(f'Found solution with usage_ratio {curr_min_usage} and slr_width_limit {slr_width_limit}')
+
+  return curr_best_v2s
+
+####################################################################################
 
 def _eight_way_partition(
   init_v2s: Dict[Vertex, Slot],
@@ -130,8 +283,8 @@ def _eight_way_partition(
   pre_assignments: Dict[Vertex, Slot],
   slot_manager: SlotManager,
   max_usage_ratio: float,
-  max_search_time: int,
   slr_width_limit: int,
+  max_search_time: int,
 ) -> Dict[Vertex, Slot]:
 
   m = get_mip_model_silent()
