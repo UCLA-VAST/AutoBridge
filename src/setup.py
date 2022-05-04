1d98ff53351de0005d4cbd218fab6f20829e8442
Licheng-Guo <lichengguo777@gmail.com>
2022-05-03 20:44:49 -0700

    build: bump version

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='autobridge',
-    version='0.0.20220502.dev1',
+    version='0.0.20220503.dev1',
     description='AutoBridge',
     long_description=long_description,
     long_description_content_type='text/markdown',
