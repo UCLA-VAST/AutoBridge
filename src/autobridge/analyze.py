48b4b60876142fa07c556d2f79afc47f6fd9970a
Licheng-Guo <lichengguo777@gmail.com>
2022-05-03 20:44:23 -0700

    log: update logging for auto hbm port binding

--- src/autobridge/analyze.py
+++ src/autobridge/analyze.py
@@ -40,6 +40,9 @@ def check_port_binding(config) -> None:
   if not config['part_num'].startswith('xcu280'):
     return
 
+  if config.get('enable_hbm_binding_adjustment', False):
+    return
+
   wrong_binding = False
   for name, prop in config['edges'].items():
     if prop['category'] == 'AXI_EDGE':
@@ -52,20 +55,21 @@ def check_port_binding(config) -> None:
 
         source = prop['path'][0]
         dst = prop['path'][1]
-        logger.critical(
-          '*** CRITICAL WARNING: '
+        logger.warning(
+          'WARNING: '
           'The top argument %s is mapped to port %s on the %s half of SLR 0. '
           'However, its adjacent module %s is floorplaned to the %s half. ',
           port_name, f'{port_cat}[{port_id}]', hbm_side, prop['consumed_by'], oppo_side)
         wrong_binding = True
 
   if wrong_binding:
-    logger.critical('')
-    logger.critical(
-      '*** CRITICAL WARNING: '
-      'Consider adjusting the port binding to balance resources on the two sides of SLR 0.'
+    logger.warning('')
+    logger.warning(
+      'WARNING: '
+      'Consider using --enable-hbm-binding-adjustment to allow automatic adjusting of the port binding '
+      'based on the floorplan results. Or manually adjust the port binding to balance resources on the two sides of SLR 0.'
     )
-    logger.critical('')
+    logger.warning('')
 
 def check_resource_usage(config):
   usage = config['slot_resource_usage']
